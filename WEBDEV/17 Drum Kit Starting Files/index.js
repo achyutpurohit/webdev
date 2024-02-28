e4de14037861b14bc58ff21f17d02806a34fcbf8
achyutpurohit <an7069@srmist.edu.in>
2024-02-28 22:54:25 +0530

    28/02/2024

---
+++ WEBDEV/17 Drum Kit Starting Files/index.js
@@ -0,0 +1,7 @@
+var noOfDrums=document.querySelectorAll(".drum").length;
+for(var i=0;i<noOfDrums;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        alert("i got clicked");
+    } );
+}
